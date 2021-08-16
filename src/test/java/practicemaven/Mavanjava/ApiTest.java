845fdcf0d559de344fef943ea8a41bcaf06b3818
dealertech84 <dealertech84@gmail.com>
2021-08-16 23:06:26 +0530

    This is commit from new workspace after updated the testcases

--- src/test/java/practicemaven/Mavanjava/ApiTest.java
+++ src/test/java/practicemaven/Mavanjava/ApiTest.java
@@ -7,6 +7,9 @@ public class ApiTest {
 	@Test
 	public void webAPI() {
 		System.out.println("webAPI");
+		System.out.println("given()");
+		System.out.println("when()");
+		System.out.println("then()");
 	} 
 	@Test
 	public void mobAPI() {
