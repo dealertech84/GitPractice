584499cfdb1112d63c8a9f7c9214b048b5f5c652
dealertech84 <dealertech84@gmail.com>
2021-08-17 13:42:50 +0530

    New branch is created branch name develop and updated the testcases

--- src/test/java/practicemaven/Mavanjava/SeleniumTest.java
+++ src/test/java/practicemaven/Mavanjava/SeleniumTest.java
@@ -7,11 +7,16 @@ public class SeleniumTest {
 	@Test
 	public void logIn() {
 		System.out.println("logIn");
+		System.out.println("UserName");
+		System.out.println("Password");
+		System.out.println("Submit");
 	}
 	
 	@Test
 	public void logOut() {
 		System.out.println("logOut");
+		System.out.println("ClickOnUserName");
+		System.out.println("Hit");
 	}
 
 }
